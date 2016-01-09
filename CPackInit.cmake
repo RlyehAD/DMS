9525653d7150dab53ef83180fbc576e4d678829f
Andrew-AbiMansour <anabiman@indiana.edu>
2016-01-09 03:57:43 -0500

    changed LGPL to GPL

--- CPackInit.cmake
+++ CPackInit.cmake
@@ -4,16 +4,16 @@
 # Copyright (c) 2014,2015 by the DMS development team.
 #
 # DMS is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public License
+# modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2.1
 # of the License, or (at your option) any later version.
 #
 # DMS is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
+# General Public License for more details.
 #
-# You should have received a copy of the GNU Lesser General Public
+# You should have received a copy of the GNU General Public
 # License along with DMS; if not, see
 # http://www.gnu.org/licenses, or write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
