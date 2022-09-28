8332a22b5c40f51e71e18c671ea0d4f8687bbd8c
RlyehAD <guo12@iu.edu>
2022-09-28 18:40:07 -0400

    Put cCoords back to Meso object. cCoords is protected

--- src/dms/classes.h
+++ src/dms/classes.h
@@ -37,7 +37,7 @@ public:
 	PetscInt Get_Dim() const { return Dim; }
 
 	CVec Get_Coords() const { return Coords; }
-	CVec Get_cCoords() const { return cCoords; } //Moved this to the son class Meso_State
+	//CVec Get_cCoords() const { return cCoords; } //Moved this to the son class Meso_State
 	CVec Get_pCoords() const { return pCoords; }
 	CVec Get_RefCoords() const { return Ref_Coords; }
 
@@ -62,7 +62,7 @@ protected:
 
 	CVec Coords,
 	     pCoords,
-	     cCoords, // used to save the constrained  
+	     //cCoords, // used to save the constrained  
 	     Velocities,
 	     pVelocities,
 	     Forces,
@@ -109,8 +109,10 @@ class Meso_state: public Micro_state {
 	std::vector< CVec > cgTensor;
 	PetscInt nHist;
 public:
+
+	//CVec cCoords;
     
-    //CVec Get_cCoords() const { return cCoords; }
+    CVec Get_cCoords() const { return cCoords; }
     
 	Meso_state(PetscInt NumCG, PetscInt DimCG, PetscInt, MPI_Comm, ptrMap, ptrMapVelo);
 
@@ -122,6 +124,10 @@ public:
 	std::vector< CVec >& getCGTensor() { return cgTensor; }
 
 	virtual ~Meso_state();
+
+protected:
+
+	CVec cCoords;
 };
 
 #endif
